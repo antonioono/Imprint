053e8165f4d62567344cb5947393e7846a9f4b64
Antonio Ono <me@antonioono.com>
2013-05-06 12:52:53 -0400

    added switching between sections

--- scripts/events.js
+++ scripts/events.js
@@ -8,4 +8,25 @@ $(document).ready(function(){
             $(".habit").not(this).addClass("closed");
         }
     });
+    
+    $(".logo").click(function(){
+        $(".habit").removeClass("open closed");
+    })
+    
+    $("nav a").click(function(){
+        if ($(this).hasClass("current")) {
+            $("nav a").not(this).addClass("current");
+            $(this).removeClass("current");
+        } else {
+            $(this).addClass("current");
+            $("nav a").not(this).removeClass("current");
+            
+            var thisId = $(this).attr("id");
+            $("section." + thisId)
+                .addClass("open").removeClass("closed");
+                
+            $("section:not(."+thisId+")")
+                .addClass("closed").removeClass("open");
+        }
+    })
 });
