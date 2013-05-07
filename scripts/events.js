57affc968f517ccdf33267c75ee39f8f7875d006
Antonio Ono <me@antonioono.com>
2013-05-06 22:14:15 -0400

    major ui overhaul
    - panel swiching ui
    - aesthetic improvments
    - img-based friends page
    - other stuff

--- scripts/events.js
+++ scripts/events.js
@@ -1,20 +1,25 @@
+var habitOpen   = false;
+
 $(document).ready(function(){
-    $(".profile.open .habit").click(function() {
+    $(".page.profile.open .habit").click(function() {
+        // already opened
         if (($(this).hasClass("closed")) || ($(this).hasClass("open"))) {
             $(this).siblings(".open").removeClass("open");
             $(this).siblings().removeClass("closed");
             $(this).removeClass("closed").removeClass("open");
-        } else {
+            habitOpen = false;
+        } else { // none opened
             $(this).addClass("open");
             $(".habit").not(this).addClass("closed");
+            habitOpen = true;
         }
     });
     
-    $(".profile.open .habit").not("open").mouseover(function() {
+    $(".page.profile.open .habit").not("open").mouseover(function() {
         $(this).siblings().addClass("other-hover");
     });
     
-    $(".profile.open .habit").not("open").mouseout(function() {
+    $(".page.profile.open .habit").not("open").mouseout(function() {
         $(".habit").removeClass("other-hover");
     });   
     
@@ -23,23 +28,57 @@ $(document).ready(function(){
     });
     
     $("nav a").click(function(){
-        if ($(this).hasClass("current")) {
-            $("nav a").not(this).addClass("current");
-            $(this).removeClass("current");
+        // if ($(this).hasClass("current")) {
+        //     null;
+        // } else {
+        //     $(this).addClass("current");
+        //     $("nav a").not(this).removeClass("current");
+        //     
+        //     var thisId = $(this).attr("id");
+        //     $("section." + thisId)
+        //         .addClass("open").removeClass("closed");
+        //         
+        //     $("section:not(."+thisId+")")
+        //         .addClass("closed").removeClass("open");
+        // }
+        // if ($(this).hasClass("profile")) {
+        //     profileOpen = false;
+        // } else {
+        //     profileOpen = true;
+        // 
+        // }
+        if ($(this).hasClass("profile")) {
+            switchSides("profile");
         } else {
-            $(this).addClass("current");
-            $("nav a").not(this).removeClass("current");
-            
-            var thisId = $(this).attr("id");
-            $("section." + thisId)
-                .addClass("open").removeClass("closed");
-                
-            $("section:not(."+thisId+")")
-                .addClass("closed").removeClass("open");
+            switchSides("friends");
+        }
+        reset();
+    });
+    
+    $(".page.closed").click(function(){
+        if ($(this).hasClass("profile")) {
+            // $(this).addClass("open").removeClass("closed");
+            // $(".page.friends").addClass("closed").removeClass("open");
+            // alert("foo")
+            switchSides("profile");
+            reset();
+        } else if ($(this).hasClass("friends")) {
+            switchSides("friends");
+            // $(this).addClass("open").removeClass("closed");
+            // $(".page.profile").addClass("closed").removeClass("open");
         }
     });
     
-    $(".friends .profile.closed").click(function(){
-        $(this).removeClass("closed").addClass("open");
-    });
+    function switchSides(side){
+        if (side === "friends") {
+            $("body").addClass("friends").removeClass("profile");
+        } else {
+            $("body").addClass("profile").removeClass("friends");
+        }
+        reset();
+    }
+    
+    function reset() {
+        $(".page.profile .habit").removeClass("open").removeClass("closed");
+    }
 });
